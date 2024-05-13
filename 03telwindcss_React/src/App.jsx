94e71957dd8e65351e1d979c0b5a78c48a542cb9
Anand Kumar Suman <anandraj0705@gmail.com>
2024-05-13 14:19:48 +0530

    caount

---
+++ 03telwindcss_React/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const name= "Anand";
+  // const photo = "maine khicha"
+  let myObj={
+    username: "Anand"
+  }
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-6'>Teilwind Test</h1>
+    <Card username="The Coldest Sunset"/>
+    <Card username="The Lovly Sunset" />
+    </>
+  )
+}
+
+export default App
